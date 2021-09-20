7fb569ff0736e1f2b01cffddf17524bdf0d7527e
lqd1434 <1434288209@qq.com>
2021-09-20 20:29:31 +0800

    fix:修复我的页面音乐记录区域的文字错位

--- src/pages/Mine/MusicArea.tsx
+++ src/pages/Mine/MusicArea.tsx
@@ -6,13 +6,13 @@ import { HeartIcon, TimeIcon } from '../../lib/icons'
 const MusicArea = () => {
 	return (
 		<Flex h={'15vh'} my={3} mx={3} alignItems={'center'} justifyContent={'space-between'}>
-			<Flex ml={2} w={'40%'} className={styles.musicCard} alignItems={'center'}>
-				<Box alignItems={'center'} p={3}>
+			<Flex ml={2} w={'45%'} className={styles.musicCard} alignItems={'center'}>
+				<Box p={3}>
 					<Box bg={'#F0FCFF'} borderRadius={10} p={3}>
 						<HeartIcon />
 					</Box>
 				</Box>
-				<Box>
+				<Box flex={1}>
 					<Flex flexDirection={'column'} ml={3} color={'#5F2FFF'} alignItems={'center'} pr={10}>
 						<Text fontWeight={500}>喜欢</Text>
 						<Text fontSize={14} color={'#3A00FB'}>
@@ -21,7 +21,7 @@ const MusicArea = () => {
 					</Flex>
 				</Box>
 			</Flex>
-			<Flex w="40%" className={styles.musicCard} mr={5} alignItems={'center'}>
+			<Flex w="45%" className={styles.musicCard} mr={5} alignItems={'center'}>
 				<Box alignItems={'center'} p={3}>
 					<Box bg={'#F0FCFF'} borderRadius={10} p={3}>
 						<TimeIcon />
