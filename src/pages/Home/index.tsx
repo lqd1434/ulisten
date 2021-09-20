39ef356fc066ad631633c3cac67d38d81c73d8c5
lqd1434 <1434288209@qq.com>
2021-09-20 18:05:39 +0800

    feat:首页布局绘制完成

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import { Box, Flex, Text } from '@chakra-ui/react'
+import { Box, Flex, SimpleGrid, Square, Text } from '@chakra-ui/react'
 import styles from './index.module.scss'
 import { MusicIcon, SearchIcon } from '../../lib/icons'
 
 const Home = () => {
 	return (
 		<Flex
-			color="white"
 			w={'100vw'}
 			height={'calc(100vh - 80px)'}
 			flexDirection={'column'}
-			alignItems={'start'}
+			alignItems={'center'}
+			color={'#1A55E9'}
 			justifyContent={'start'}
 		>
 			<Flex w={'100vw'} mt={'1rem'} alignItems={'center'}>
@@ -22,7 +22,7 @@ const Home = () => {
 						</Text>
 					</Flex>
 				</Box>
-				<Box flex={1} className={styles.title} mx={4} h={'3rem'}></Box>
+				<Box flex={1} className={styles.title} mx={4} h={'3rem'} />
 				<Flex
 					mr={4}
 					className={styles.numui}
@@ -34,6 +34,38 @@ const Home = () => {
 					<SearchIcon />
 				</Flex>
 			</Flex>
+			<Flex
+				ml={4}
+				mt={8}
+				alignSelf={'start'}
+				fontSize={14}
+				w={'90vw'}
+				justifyContent={'space-between'}
+			>
+				<Text className={styles.numui} py={1} px={3}>
+					歌单速递
+				</Text>
+				<Text className={styles.numui} py={1} px={3}>
+					更多
+				</Text>
+			</Flex>
+			<Box mt={'1.1rem'} w={'95vw'} h={'35vw'} className={styles.numuiCollapse} p={4}>
+				<SimpleGrid justifyItems={'center'} columns={3} mt={1}>
+					<Square className={styles.numui} h={'25vw'} w={'25vw'}>
+						1
+					</Square>
+					<Square className={styles.numui} h={'25vw'} w={'25vw'}>
+						2
+					</Square>
+					<Square className={styles.numui} h={'25vw'} w={'25vw'}>
+						3
+					</Square>
+				</SimpleGrid>
+			</Box>
+			<Box ml={4} mt={6} alignSelf={'start'} className={styles.numui} fontSize={14} py={1} px={3}>
+				新歌推荐
+			</Box>
+			<Box flex={1} mt={'1rem'} mb={'1rem'} className={styles.numuiCollapse} w={'95vw'} />
 		</Flex>
 	)
 }
